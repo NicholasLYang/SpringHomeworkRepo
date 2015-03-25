1015dff2c70ad6f4371d8e8e7ab1a4c993b45fe1
Nicholas Yang <nick@nicholasyang.com>
2015-03-25 08:41:20 -0400

    added hw 11

---
+++ 11/Queue.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class Queue<E>
+{
+    Node<E> front, back;
+    public Queue (Node<E> N)
+    {
+	front = N;
+	Node<E> tmp = N;
+	while (tmp.getNext() != null)
+	    {
+		tmp = tmp.getNext();
+	    }
+	back = tmp;
+		    
+    }
+    public Queue()
+    {
+	front = new Node<E>();
+	back = new Node<E>();
+	front.setNext(back);
+    }
+    public void enqueue(E x)
+    {
+	Node<E> n = new Node<E>(x);
+	back.setNext(n);
+	back = n;
+    }
+    public E dequeue()
+    {
+	Node<E> tmp = front;
+	front = front.getNext();
+	return tmp.getData();
+    }
+    public boolean empty()
+    {
+	return front == null;
+    }
+    public E head()
+    {
+	return front.getData();
+    }
+
+}
